7e36bac8602f99d3d0e68a395b9cfe458f45e01e
l4mb0 <juan.navarro@utec.edu.pe>
2020-07-17 22:54:59 -0500

    pls work

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+const port = process.env.PORT || 3000
+
 const cors = require('cors')
 app.use(cors())
 
@@ -8,7 +10,6 @@ app.get('/api', (req, res) => {
   res.status(200).json({api: 'version 1'})
 })
 
-const port = process.env.PORT || 3000
 app.listen(port, () => console.log('server started on port', port))
 
 
