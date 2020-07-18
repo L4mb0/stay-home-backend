b4a40a7567c551f1750dd59a16fab318cae177db
l4mb0 <juan.navarro@utec.edu.pe>
2020-07-17 22:50:18 -0500

    backend test

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+const cors = require('cors')
+app.use(cors())
+
+app.get('/api', (req, res) => {
+  res.status(200).json({api: 'version 1'})
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log('server started on port', port))
+
+
